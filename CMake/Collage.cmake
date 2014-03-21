beb8727c5fa72b9af65381256c580ed3b5c49aea
carlosduelo <carlos.duelo@gmail.com>
2014-03-21 11:48:23 +0100

    mpi dependency is optional

--- CMake/Collage.cmake
+++ CMake/Collage.cmake
@@ -14,7 +14,7 @@
 
 
 set(COLLAGE_PACKAGE_VERSION 1.1.0)
-set(COLLAGE_DEPENDS OFED UDT REQUIRED Boost Lunchbox MPI)
+set(COLLAGE_DEPENDS OFED UDT MPI REQUIRED Boost Lunchbox)
 set(COLLAGE_DEB_DEPENDS librdmacm-dev libibverbs-dev librdmacm-dev libudt-dev
   libboost-date-time-dev libboost-regex-dev libboost-serialization-dev
   libboost-system-dev libboost-program-options-dev)
